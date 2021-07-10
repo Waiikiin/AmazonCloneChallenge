9a762fdd3463fa94dabee9b8af7f34b190a8af18
Wai Kin Leong <wai.kin.leong3412@gmail.com>
2021-07-10 16:21:12 -0500

    set up project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDpmHmmP38kTcM7EmxBYnaG2X3hD0V4iQY",
+    authDomain: "clone-93721.firebaseapp.com",
+    projectId: "clone-93721",
+    storageBucket: "clone-93721.appspot.com",
+    messagingSenderId: "1000218767989",
+    appId: "1:1000218767989:web:9e9a5bf1f36e0dc793fe3c",
+    measurementId: "G-DBY5CQTV1K"
+};
