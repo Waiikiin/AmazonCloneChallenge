3c897b07717d83d49b7fc46d5cfa468d332b9d85
Wai Kin Leong <wai.kin.leong3412@gmail.com>
2021-07-18 14:08:38 -0500

    Restyled the product layout * added more products * product info is now under the product image

--- src/Home.js
+++ src/Home.js
@@ -14,25 +14,53 @@ function Home(props) {
 
                 <div className="home_row">
                     <Product 
-                    id= "123123"
-                    title={"The ultimate gaming PC"} 
-                    price={1499.99}
-                    image="https://www.downloadclipart.net/large/gaming-computer-png-hd.png"
+                    id= "1"
+                    title={"[2021 Upgraded Version] Wireless Switch Controller for N-Switch/ Switch Lite, RegeMoudal Switch Controller Gamepad Remote Gamepad Joystick"} 
+                    price={24.59}
+                    image="https://m.media-amazon.com/images/I/71BSd-0e5OL._AC_SL1500_.jpg"
+                    rating={5}/>
+                    <Product 
+                    id= "2"
+                    title={"Canon PowerShot G7 X Mark II Digital Camera, Black"} 
+                    price={645.99}
+                    image="https://m.media-amazon.com/images/I/416b8xNfDJL._AC_.jpg"
+                    rating={4}/>
+                </div>
+
+                <div className="home_row">
+                    <Product 
+                    id= "3"
+                    title={"All-new Echo Dot (4th Gen) | Smart speaker with Alexa | Charcoal"} 
+                    price={69.99}
+                    image="https://m.media-amazon.com/images/I/71fnXKyRa4L._AC_SL1000_.jpg"
                     rating={4}/>
                     <Product 
-                    id= "1231234"
-                    title={"The ultimate gaming PC 2"} 
-                    price={1399.99}
-                    image="https://www.downloadclipart.net/large/gaming-computer-png-hd.png"
+                    id= "4"
+                    title={"Garmin Vivofit 3 Activity Tracker, Regular Fit-Black"} 
+                    price={89.95}
+                    image="https://m.media-amazon.com/images/I/81re7MNoK5L._AC_SL1500_.jpg"
+                    rating={3}/>
+                    <Product 
+                    id= "5"
+                    title={"BedStory Mattress Topper, Gel-Infused Memory Foam Pillow Top Fiber Mattress"} 
+                    price={127.49}
+                    image="https://m.media-amazon.com/images/I/71YbChxzE-L._AC_SL1500_.jpg"
                     rating={4}/>
                 </div>
 
                 <div className="home_row">
-                    {/* Product */}
-                </div>
-
-                <div className="home_row">
-                    {/* Product */}
+                <Product 
+                    id= "6"
+                    title={"AICOOK Bread Maker, 2LB Stainless Steel Bread Machine with Gluten-Free Setting, Fruit Nut Dispenser, Large LCD display, Nonstick Pan, 3 Crust Colors & Keep Warm Set, Recipes"} 
+                    price={109.99}
+                    image="https://m.media-amazon.com/images/I/717qVZN2VwL._AC_SL1493_.jpg"
+                    rating={4}/>
+                    <Product 
+                    id= "7"
+                    title={"Sony WH-1000XM4 Wireless Industry Leading Noise Canceling Overhead Headphones, Black, One Size (WH1000XM4/B)"} 
+                    price={398.00}
+                    image="https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg"
+                    rating={4}/>
                 </div>
             </div>
         </div>
