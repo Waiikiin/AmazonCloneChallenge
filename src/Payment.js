3955aa095a3c3f5ce2ca9526e70661e9a9432752
Wai Kin Leong <wai.kin.leong3412@gmail.com>
2021-07-18 10:04:30 -0500

    added stripe functionality for payment processing added axios for fetch functionality * axios isn't fully set up yet

--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 import CheckoutProduct from './CheckoutProduct'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import CurrencyFormat from 'react-currency-format'
+import { getBasketTotal } from './reducer'
+import axios from './axios'
 
 function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
+    const history = useHistory();
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [error, setError ] = useState(null);
+    const [succeeded, setSucceeded ] = useState(false);
+    const [processing, setProcessing ] = useState(false);
+    const [disabled, setDisabled ] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
+
+    // every time the component loads/cart items change, it calls this method
+    useEffect(() => {
+        //generate the special stripe secret which allows us to charge a customer
+        const getClientSecret = async () => {
+            const response = await axios ({
+                method: 'post',
+                // Stripe expects the total in a currencies subunits
+                url: `/payment/create?total=${getBasketTotal(basket)* 100}`
+            })
+            setClientSecret(response.data.clientSecret)
+        }
+        return () => {
+            //cleanup
+        }
+    }, [basket])
+
+    const handleSubmit = async event => {
+        // stipe processing
+        event.preventDefault();
+        setProcessing(true);
+
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement)
+            }
+        }).then(( {paymentIntent}) => {
+            // paymentIntent = payment confirmation
+            setSucceeded(true);
+            setProcessing(false);
+            setError(null);
+
+            history.replace('/orders')
+        })
+    }
+
+    const handleChange = event => {
+        // stipe processing
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : "");
+    }
 
     return (
         <div className="payment">
@@ -52,8 +107,32 @@ function Payment() {
 
                     <div className="payment_details">
                         {/*stripe*/}
+
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
+
+                            <div className="payment_priceContainer">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3> Order Total: {value} </h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled ||
+                                succeeded}>
+                                    <span> {processing ? <p>Processing</p> 
+                                    : "Buy Now"} </span>
+                                </button>
+                            </div>
+                            
+                            {/* Errors */}
+                            {error && <div>{error}</div>}
+                        </form>
                     </div>
-                    {/*payment method*/}
                 </div>
             </div>
         </div>
